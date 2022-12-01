48751cb23d7754a69b6b7d6080b9d2814c62e2e8
Leonardo Simeone <leo_sime@yahoo.com>
2022-12-01 16:50:29 +0000

    Add click event listeners for buttons and images

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,20 @@
 
 let userChoice = "";
 let computerChoice = "";
+/**
+ * Once the DOM is loaded it gets button elements as an array,
+ * "loops" through them and listens for user clicks
+ * then it assigns the right value and image to the userChoice variable
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    getModal();
+    let buttons = document.getElementsByClassName("btn");
+    for (let button of buttons) {
+        button.addEventListener("click", btnClicked);
+    }
+
+    let images = document.getElementsByClassName("images");
+    for (let image of images) {
+        image.addEventListener("click", imgClicked);
+    }
+});
