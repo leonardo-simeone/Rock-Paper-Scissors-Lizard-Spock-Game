6b59667ff9d1bf6dbb0bf25a970675f4c4b87a1a
Leonardo Simeone <leo_sime@yahoo.com>
2022-12-01 16:58:05 +0000

    Create assign variable to button click function

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,45 @@ document.addEventListener("DOMContentLoaded", function () {
         image.addEventListener("click", imgClicked);
     }
 });
+
+function btnClicked(e) {
+    let btn = e.target;
+    if (btn === document.getElementById("rock")) {        
+
+        userChoice = "rock";
+        document.getElementById("user-image").src = "assets/images/rock.png";
+        document.getElementById("user-image").alt = "rock image";
+        document.getElementById('user-image').setAttribute('aria-label', 'user selected rock');
+
+    } else if (btn === document.getElementById("paper")) {
+
+        userChoice = "paper";
+        document.getElementById("user-image").src = "assets/images/paper.png";
+        document.getElementById("user-image").alt = "paper image";
+        document.getElementById('user-image').setAttribute('aria-label', 'user selected paper');
+        
+    } else if (btn === document.getElementById("scissors")) {
+
+        userChoice = "scissors";
+        document.getElementById("user-image").src = "assets/images/scissors.png";
+        document.getElementById("user-image").alt = "scissors image";
+        document.getElementById('user-image').setAttribute('aria-label', 'user selected scissors');
+        
+    } else if (btn === document.getElementById("lizard")) {
+
+        userChoice = "lizard";
+        document.getElementById("user-image").src = "assets/images/lizard.png";
+        document.getElementById("user-image").alt = "lizard image";
+        document.getElementById('user-image').setAttribute('aria-label', 'user selected lizard');
+        
+    } else {
+
+        userChoice = "spock";
+        document.getElementById("user-image").src = "assets/images/spock.png";
+        document.getElementById("user-image").alt = "spock image";
+        document.getElementById('user-image').setAttribute('aria-label', 'user selected spock');
+        
+    }    
+    console.log(userChoice);
+    runGame();    
+}
